45453b5df43d9e1edcc189ab79bcbec1007aca09
Sanam Kharbat <Sanam.Kharbat@xcelserv.com>
2016-11-17 16:06:58 +0530

    Changes 17-11-2016

--- app/code/local/Mageplaza/ProductSlider/Model/Mysql4/Productslider.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Mageplaza_ProductSlider_Model_Mysql4_Productslider extends Mage_Core_Model_Mysql4_Abstract
-{
-    public function _construct()
-    {
-        $this->_init('productslider/productslider', 'productslider_id');
-    }
-}
--- skin/frontend/accessshop/default/js/custom.js
+++ skin/frontend/accessshop/default/js/custom.js
@@ -1,57 +1,74 @@
-   jQuery(document).ready(function() {
-     	
-  jQuery('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:0,
-    nav:true,
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true,
-    autoplaySpeed: 1000,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
-    }
-});
-//var owl = $("#owl-demo");
- 
-  // owl.owlCarousel({
-      // items : 10, //10 items above 1000px browser width
-      // itemsDesktop : [1000,4], //5 items between 1000px and 901px
-      // itemsDesktopSmall : [900,3], // betweem 900px and 601px
-      // itemsTablet: [600,2], //2 items between 600 and 0
-      // itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
-  // });
+jQuery(document).ready(function() {
 
-
-jQuery('.fourpice .owl-carousel1').owlCarousel({
-   loop:true,
-    margin:-70,
-   nav:true,
-   autoplay:true,
-   //autoplayTimeout:3000,
-   autoplayHoverPause:true,
-   autoplaySpeed: 1000,
-   responsive:{
-       0:{
-           items:1
-       },
-       600:{
-           items:2
-       },
-       1000:{
-           items:4
-       }
-   }
+	jQuery('.owl-carousel').owlCarousel({
+		loop : true,
+		margin : 0,
+		nav : true,
+		autoplay : true,
+		autoplayTimeout : 3000,
+		autoplayHoverPause : true,
+		autoplaySpeed : 1000,
+		responsive : {
+			0 : {
+				items : 1
+			},
+			600 : {
+				items : 1
+			},
+			1000 : {
+				items : 1
+			}
+		}
+	});
+	jQuery('.owl-bannerWarp').owlBanner({
+		loop : true,
+		margin : 10,
+		nav : false,
+		autoplay : true,
+		autoplayTimeout : 3000,
+		autoplayHoverPause : true,
+		autoplaySpeed : 1000,
+		responsive : {
+			0 : {
+				items : 1
+			},
+			600 : {
+				items : 3
+			},
+			1000 : {
+				items : 4
+			}
+		}
+	});
+	var owl = jQuery('.owl-bannerWarp');
+	owl.owlBanner();
+	jQuery('.customNextBtn').click(function() {
+		owl.trigger('next.owl.carousel');
+	});
+	jQuery('.customPrevBtn').click(function() {
+		owl.trigger('prev.owl.carousel', [300]);
+	})
 });
 
-    });
-    
-    
+// jQuery('.fourpice .owl-carousel1').owlCarousel({
+// loop:true,
+// margin:-70,
+// nav:true,
+// autoplay:true,
+// //autoplayTimeout:3000,
+// autoplayHoverPause:true,
+// autoplaySpeed: 1000,
+// responsive:{
+// 0:{
+// items:1
+// },
+// 600:{
+// items:2
+// },
+// 1000:{
+// items:4
+// }
+// }
+// });
+
+
